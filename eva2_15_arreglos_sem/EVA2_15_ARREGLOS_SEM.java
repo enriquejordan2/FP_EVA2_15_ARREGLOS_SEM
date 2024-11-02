52fb71037c0fa01cce50d41129e91137feebb6b5
enriquejordan2 <L24550342@chihuahua2.tecnm.mx>
2024-11-02 11:00:19 -0600

    Add files via upload

---
+++ eva2_15_arreglos_sem/EVA2_15_ARREGLOS_SEM.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva2_15_arreglos_sem;
+
+/**
+ *
+ * @author Jose Enrique
+ */
+public class EVA2_15_ARREGLOS_SEM {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        String[]diasemana = new String[7];
+        diasemana [0]= "Domingo";
+        diasemana [1]= "Lunes";
+        diasemana [2]= "Martes";
+        diasemana [3]= "Miercoles";
+        diasemana [4]= "Jueves";
+        diasemana [5]= "Viernes";
+        diasemana [6]= "Sabado";
+        
+        //Imprimir
+        for(int i =0; i <diasemana.length; i++){
+            System.out.println(diasemana[i]);
+        }
+      
+    }
+    
+}
